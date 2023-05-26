529c9af7c5a02ade553d3b672d9cbf00473a816d
JesusRosml <jesusperezhidalgo50@gmail.com>
2023-05-26 02:26:47 -0600

    Son las 3 am

--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const Main = () => {
+
     return (
         <SafeAreaView style = {{ flex: 1 }}>
 
             <View style={styles.container}>
-                <View style = {[styles.bottomView, {backgroundColor: "#BDBDBD", height: 280,}]}>
-                    <Text>SocialLife</Text>
+                <View style = {[styles.topView, {height: 50, flexDirection: "row", justifyContent: "space-between", alignItems: "center"}]}>
+                    <TouchableOpacity>
+                        <Icon size={24} name="bars" solid style={styles.icon}/>
+                    </TouchableOpacity>
+
+                    <Icon size={28} name="bell" solid style={styles.iconBell}/>
+                    <Image 
+                        source={require("./img/gatitogumbal.jpg")}
+                        style={[styles.imagen, { width: 36, height: 36, borderRadius: 50 }]}
+                    />
+                </View>
+                <View style = {[styles.bottomView, {backgroundColor: "#BDBDBD", height: 300,}]}>
                     <MapView
-                    style={{ flex: 1 }} // Mover el corchete de cierre aquí
+                    style={{ flex: 1,}} // Mover el corchete de cierre aquí
                     provider={PROVIDER_GOOGLE}
                     showsUserLocation
                     initialRegion={{
@@ -22,31 +34,35 @@ const Main = () => {
                     }}
                 />
                 </View>
-                
-                <View style = {[styles.topView, {backgroundColor: "#E0E0E0", height: 45}]}>
-                    <Text>SocialLife</Text>
-                </View>
             </View>
-
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create ({
+    icon: {
+        fontFamily: "FontAwesome5ProThin",
+        marginLeft: 15,
+      },
+    iconBell: {
+        marginLeft: 220,
+    },
+    imagen: {
+        marginRight: 15,
+    },
     container: {
         flex: 1,
       },
       bottomView: {
-        flex: 1,
         backgroundColor: "blue",
       },
       topView: {
         position: "absolute",
+        display: "flex",
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: "red",
         zIndex: 1,
       },
 })
